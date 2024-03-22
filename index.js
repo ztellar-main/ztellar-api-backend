cd1200737399853e6002d164ebf0d2c573dd4de0
Ichiden <denverbigayan1@gmail.com>
2024-03-22 20:25:37 +0800

    03/22/2024b

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin:["http://localhost:3000"],
+    origin:["https://ztellar-5r2r.onrender.com"],
     credentials:true
 }));
 
