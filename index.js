a739c331ca0cd9e116929bed10ee0dd5a9b0cd50
ztellar-main <164524295+ztellar-main@users.noreply.github.com>
2024-04-02 17:29:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin:["https://ztellar-7rf8.onrender.com"],
+    origin:["https://ztellar-7rf8.onrender.com","https://ztellar.tech"],
     credentials:true
 }));
 
