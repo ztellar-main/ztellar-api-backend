243ee25a34346ecfa713fface73347c0258cd955
Ichiden <denverbigayan1@gmail.com>
2024-08-30 20:01:06 +0800

    asd

--- controllers/convertVideoController.ts
+++ controllers/convertVideoController.ts
@@ -6,6 +6,7 @@ import { ERROR_HANDLER } from '../constants/errorCodes';
 import { convertToHLSAndUpload } from '../utils/videoConverter';
 import path from 'path';
 import fs from 'fs';
+import { errorMonitor } from 'stream';
 
 export interface IGetUserAuthInfoRequest extends Request {
   user: any; // or any other type
@@ -16,7 +17,11 @@ export const convertVideoToHls = tryCatch(
   async (req: IGetUserAuthInfoRequest, res: Response) => {
     const { videoUrl, filePath, socketId, fileName } = req.body;
     //   `course/converted/${courseId}/video_intro/denver`;
-    await convertToHLSAndUpload(videoUrl, filePath, fileName);
-    res.json('success');
+    try {
+      await convertToHLSAndUpload(videoUrl, filePath, fileName);
+      res.json('success');
+    } catch (err) {
+      res.json(err);
+    }
   }
 );
